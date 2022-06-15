a14fd446c75f548fecf075ad87241d2fe6022a4e
Swan <adeliine.0618@gmail.com>
2022-06-16 01:48:42 +0900

    learn routing

--- routing/app.js
+++ routing/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-
+const admin = require('./routes/admin');
+const contacts = require('./routes/contacts');
 const app = express();
 const port = 3000;
 
@@ -9,6 +10,11 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('hello express');
 });
+app.get('/Swan', (req, res) => {
+  res.send('hello Sion' + ' hello Woochoo');
+});
+app.use('/admin', admin);
+app.use('/contacts', contacts);
 
 //포트 지정.  == http.createServer().listen(3000)
 app.listen(port, () => {
