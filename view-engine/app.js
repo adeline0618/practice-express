58f5534259b61b8f186a0a1b45bdd4d73dbe7951
Swan <adeliine.0618@gmail.com>
2022-06-17 01:05:05 +0900

    view-engine

--- view-engine/app.js
+++ view-engine/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const admin = require('./routes/admin');
 const contacts = require('./routes/contacts');
+const nunjucks = require('nunjucks');
+
 const app = express();
 const port = 3000;
 
 //req : 사용자의 리퀘스트
 //res : 내가 보여줄 response
-
+nunjucks.configure('template', {
+  autoescape: true,
+  express: app, //express()객체 할당한 변수 명.
+});
 app.get('/', (req, res) => {
   res.send('hello express');
 });
@@ -20,3 +25,5 @@ app.use('/contacts', contacts);
 app.listen(port, () => {
   console.log('Express listening on port', port);
 });
+
+// get 요청 : 클라이언트가 웹브 URL창에 쳤을 때
